11e8b0364db3a6d6269592eb4e6992426990337d
Ben Griffis <griffis.ben16@gmail.com>
2024-03-26 15:55:32 -0400

    Update best_11_role_ranks.py

--- RoleRanksApp/best_11_role_ranks.py
+++ RoleRanksApp/best_11_role_ranks.py
@@ -675,7 +675,7 @@ clean_df = load_league_data(df, f"{lg} {season}")
 
 with st.sidebar:
     one_team_choice = st.selectbox('One Team Depth Chart?', (['No','Yes']))
-    if one_choice_team == 'Yes':
+    if one_team_choice == 'Yes':
         chosen_team = st.selectbox('League', (clean_df['Team within selected timeframe'].tolist()))
     else:
         chosen_team = 'N/A'

1feb8fee54cc518d44dddc60238ef70e849ff45f
Ben Griffis <griffis.ben16@gmail.com>
2024-04-29 11:32:20 -0400

    Update PostMatch_Report_App.py

--- PostMatchApp/PostMatch_Report_App.py
+++ PostMatchApp/PostMatch_Report_App.py
@@ -30,7 +30,7 @@ with st.sidebar:
         render_matches = match_list.head(num_matches).Match.tolist()
 
 for i in range(len(render_matches)):
-    game_image = Image.open(f"https://raw.githubusercontent.com/griffisben/misc-code/main/PostMatchApp/USLC_2024/{render_matches[i]}.png")
+    game_image = Image.open(f"/PostMatchApp/USLC_2024/{render_matches[i]}.png")
     st.image(game_image)
 
 

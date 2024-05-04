c04adebe63a22d1d857c88a3d4678fe36381af06
Ben Griffis <griffis.ben16@gmail.com>
2024-05-03 20:15:40 -0400

    Update best_11_role_ranks.py

--- RoleRanksApp/best_11_role_ranks.py
+++ RoleRanksApp/best_11_role_ranks.py
@@ -591,7 +591,8 @@ def make_rankings(formation, mins, data, role_position_df, leagues, exp_contract
             if exp_contracts == 'y':
                 ranks = ranks[ranks['Contract expires'].isin(exp_dates)]
             ranks = ranks[ranks['Age'].between(min_age,max_age)]
-    
+            ranks['Formation Pos.'] = formation_positions[formation][q]
+            
             rank_list = pd.concat([rank_list,ranks])
     
     rank_list.Age = rank_list.Age.astype(int)
@@ -687,10 +688,6 @@ with st.sidebar:
 rank_list = make_rankings(formation, mins/100, clean_df, role_position_df, [lg], exp_contracts, expiration_date,
                           min_age=ages[0], max_age=ages[1], num=number_of_players, normalize_to_100=normalize_to_100, chosen_team=chosen_team)
 show_ranks = rank_list[['Player','Team','Age','Squad Position','Player Pos.','Score','Role Rank']].copy()
-pitch_pos = []
-for i in range(0,11):
-    pitch_pos += [formation_positions[formation][i]]*number_of_players
-show_ranks['Formation Pos.'] = pitch_pos
 
 
 

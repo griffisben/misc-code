19b19b1e15907bb55f28e00007ece0e9fdc522eb
Ben Griffis <griffis.ben16@gmail.com>
2024-08-06 08:30:32 -0400

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
-st.video("https://1726397843.rsc.cdn77.org/live/a8d4dff0-d37a-4f9b-b3a6-a583fb80d31c/video_2.m3u8", format='video/m3u8')
+video_file = open("https://1726397843.rsc.cdn77.org/live/a8d4dff0-d37a-4f9b-b3a6-a583fb80d31c/video_2.m3u8", 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)

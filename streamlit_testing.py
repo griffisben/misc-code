0d14b1b4eb682e7f999311af02367d008f758ce8
Ben Griffis <griffis.ben16@gmail.com>
2024-08-06 09:10:23 -0400

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-components.iframe("""
+components.html("""
 <iframe width="560" height="315" src="https://www.youtube.com/embed/A1W3qYAy5G4?si=7gK6QgSdbN5MtlTA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 """
                  )

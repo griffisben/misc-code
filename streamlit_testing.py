92c6dfe83ec27f738c8d287f30475e1f57480391
Ben Griffis <griffis.ben16@gmail.com>
2024-08-06 08:31:37 -0400

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -1,3 +1,3 @@
 import streamlit as st
 
-st.video("https://1726397843.rsc.cdn77.org/live/a8d4dff0-d37a-4f9b-b3a6-a583fb80d31c/video_2.m3u8")
+st.video("https://ekstraklasa.tv/en/watch/cracovia-widzew-lodz-3-kolejka")

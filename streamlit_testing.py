1a3373ad54cad9fb29debf4760ac61701edba1d3
Ben Griffis <griffis.ben16@gmail.com>
2024-11-22 20:31:24 -0500

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -10,6 +10,31 @@ import matplotlib.patheffects as path_effects
 from adjustText import adjust_text
 plt.rcParams['figure.dpi'] = 300
 
+def color_percentile(pc):
+    if 1-pc <= 0.1:
+        color = ('#01349b', '#d9e3f6')  # Elite
+    elif 0.1 < 1-pc <= 0.35:
+        color = ('#007f35', '#d9f0e3')  # Above Avg
+    elif 0.35 < 1-pc <= 0.66:
+        color = ('#9b6700', '#fff2d9')  # Avg
+    else:
+        color = ('#b60918', '#fddbde')  # Below Avg
+
+    return f'background-color: {color[1]}'
+
+with st.expander('Information about Roles & Action Values'):
+    st.write("""
+    Role Clusters determined by an Algorithm trained on heatmaps of all UEFA T5 league players, over 2.9 million events.  \n
+    Players are assigned a cluster based on the areas they operate in. Clusters are subjectively named by me.  \n
+    This aims to describe the areas of the pitch a player operates in, instead of 'playmaker', 'false 9', etc.  \n
+      \n
+    Action Value is similar to StatsBomb's On-Ball Value (OBV) or American Soccer Analysis's Goals Added (g+).  \n
+    This metric values each action in terms of the change in probability of scoring & conceding (proxied by xG instead of actual goals).  \n
+    The model looks at the action type, where it took place, and context variables such as phase of play & other actions in the possession.  \n
+    It excludes all penalties, totals each player's attacking & defensive value, calculates value/90', and then finally  \n
+    compares this per 90 number to their position's average score. Their position is determined by the Clustering model.  \n
+    The positions are shows under the player names, before the clustering role. So, a CM with a positive value may be better than the average CM
+    """)
 
 lg_lookup = pd.read_csv("https://raw.githubusercontent.com/griffisben/misc-code/main/VAEP/VAEP_app_leagues.csv")
 league_list = lg_lookup.League.unique().tolist()
@@ -38,21 +63,11 @@ position_avg = adj_clusters.groupby('Group')['VAEP/90'].mean().rename('Group_Avg
 adj_clusters = adj_clusters.merge(position_avg, on='Group')
 adj_clusters['VAEP/90 vs Group Avg'] = adj_clusters['VAEP/90'] - adj_clusters['Group_Avg']
 adj_clusters = adj_clusters.sort_values(by=['VAEP/90 vs Group Avg'],ascending=False).reset_index(drop=True)
+adj_clusters['P_goal_diff/90'] = adj_clusters['P_goal_diff']/(adj_clusters['Minutes']/90)
+adj_clusters['P_concede_diff/90'] = adj_clusters['P_concede_diff']/(adj_clusters['Minutes']/90)
 
 #################################################################################################################
-def VAEP_team_img(team,clusters,min_mins,max_mins,minimum_minutes,sub_title):
-    ending_note = f"""Role Clusters determined by an Algorithm trained on heatmaps of all UEFA T5 league players, over 2.9 million events.
-    Players are assigned a cluster based on the areas they operate in. Clusters are subjectively named by me.
-    This aims to describe the areas of the pitch a player operates in, instead of 'playmaker', 'false 9', etc.
-    
-    Action Value is similar to StatsBomb's On-Ball Value (OBV) or American Soccer Analysis's Goals Added (g+).
-    This metric values each action in terms of the change in probability of scoring & conceding (proxied by xG instead of actual goals).
-    The model looks at the action type, where it took place, and context variables such as phase of play & other actions in the possession.
-    It excludes all penalties, totals each player's attacking & defensive value, calculates value/90', and then finally
-    compares this per 90 number to their position's average score. Their position is determined by the Clustering model.
-    The positions are shows under the player names, before the clustering role. So, a CM with a positive value may be better than the average CM"""
-    
-    
+def VAEP_team_img(team,clusters,min_mins,max_mins,minimum_minutes,sub_title):    
     pitch = Pitch(pitch_type='opta', pitch_color='#fbf9f4', line_color='#4A2E19', line_zorder=2,half=False)
     fig, axs = pitch.grid(endnote_height=0.045, endnote_space=0, figheight=12,
                           title_height=0.045, title_space=0,
@@ -89,8 +104,8 @@ def VAEP_team_img(team,clusters,min_mins,max_mins,minimum_minutes,sub_title):
     axs['endnote'].text(1, 0.5, "@BeGriffis", ha='right', va='center', size=17, color='#4a2e19')
     # axs['endnote'].text(0.5, 1, f"Role Clusters determined by an Algorithm trained on heatmaps of all UEFA T5 league players, over 2.9 million events.\nPlayers are assigned a cluster based on the areas they operate in. Clusters are subjectively named by me.\nThis aims to describe the areas of the pitch a player operates in, instead of 'playmaker', 'false 9', etc.\n\nAction Value is similar to StatsBomb's On-Ball Value (OBV) or American Soccer Analysis's Goals Added (g+).\nThis metric values each action in terms of the change in probability of scoring & conceding (proxied by xG instead of actual goals).\nThe model looks at the action type, where it took place, and context variables such as phase of play & other actions in the possession.\nIt excludes all penalties, totals each player's attacking & defensive value, calculates value/90', and then finally\ncompares this per 90 number to their position's average score. Their position is determined by the Clustering model.\nThe positions are shows under the player names, before the clustering role. So, a CM with a positive value may be better than the average CM",
     #                     ha='center', va='top', size=13, color='#4a2e19')
-    axs['endnote'].text(0.5, 1, ending_note,
-                        ha='center', va='top', size=13, color='#4a2e19')
+    # axs['endnote'].text(0.5, 1, ending_note,
+    #                     ha='center', va='top', size=13, color='#4a2e19')
     
     ##############################
     axs['pitch'].text(105, 99.75, 'AV/90\nvs Pos. Avg', ha='center', va='bottom', size=15, color='#4a2e19', weight='bold')
@@ -115,10 +130,21 @@ def VAEP_team_img(team,clusters,min_mins,max_mins,minimum_minutes,sub_title):
         axs['pitch'].text(103, ys[i], round(ticks[i],2), color=my_cmap(norm(ticks[i])), path_effects=path_eff, size=14, ha='left', va='center')
 
     return fig
-vaep_img = VAEP_team_img(team,adj_clusters,min_mins,max_mins,minimum_minutes,sub_title)
-vaep_img
 
-team_vaep_players = adj_clusters[adj_clusters.Team==team][['playerName','Team','Minutes','Desc','VAEP/90','VAEP/90 vs Group Avg','P_goal_diff','P_concede_diff']].rename(columns={
-    'playerName':'Player','Desc':'Role','P_goal_diff':'Attack Value (+)','P_concede_diff':'Defense Value (-)','VAEP_value':'VAEP'
-})
-team_vaep_players
+team_tab, player_tab = st.tabs([f'{team} Players', 'Player Research'])
+
+with team_tab:
+    vaep_img = VAEP_team_img(team,adj_clusters,min_mins,max_mins,minimum_minutes,sub_title)
+    vaep_img
+
+    team_vaep_players = adj_clusters[adj_clusters.Team==team][['playerName','Team','Minutes','Desc','VAEP/90','VAEP/90 vs Group Avg','P_goal_diff','P_concede_diff']].rename(columns={
+        'playerName':'Player','Desc':'Role','P_goal_diff/90':'Attack Value (+)/90','P_concede_diff/90':'Defense Value (-)/90','VAEP_value':'VAEP'
+    })
+    team_vaep_players
+
+with player_tab:
+    foc_pos = st.selectbox('Gender', ('ST','Winger','AM','CM','DM','FB','CB','GK'))
+    player_vaep_df = adj_clusters[adj_clusters.Group==foc_pos][['playerName','Team','Minutes','Desc','VAEP/90','VAEP/90 vs Group Avg','P_goal_diff','P_concede_diff']].rename(columns={
+        'playerName':'Player','Desc':'Role','P_goal_diff/90':'Attack Value (+)/90','P_concede_diff/90':'Defense Value (-)/90','VAEP_value':'VAEP'
+    })
+    player_vaep_df

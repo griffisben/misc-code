d6b8bd5ea3628b8d0499bedc1e558f408ad134cf
Ben Griffis <griffis.ben16@gmail.com>
2025-01-29 13:54:34 -0500

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -16,7 +16,6 @@ focal_position = st.sidebar.selectbox('Select the position', all_changes['Primar
 focal_metric = st.sidebar.selectbox("Select metric:", [col.replace(" Change", "") for col in all_changes.columns if "Change" in col])
 min_players = st.sidebar.slider('Minimum number of players', 1, 10, 4)
 start_metric = st.sidebar.number_input('Starting metric value', value=2.4, step=0.01)
-mins = st.sidebar.number_input('Minutes played per season', value=30*90, step=1)
     
 # Create graph
 G = nx.DiGraph(directed=True)
@@ -67,34 +66,9 @@ try:
     st.write(f"Shortest path for a {focal_position}, with at least {min_players} players for each move:")
     st.write(" -> ".join(shortest_path))
     st.write(f"Change: {round((foc_num - start_metric) / start_metric * 100, 2)}%")
-    st.write(f"Starting value: {round(start_metric, 2)}")
-    st.write(f"Target value: {round(foc_num, 2)}")
-    st.write(f"Starting season: {round(start_metric * (mins / 90), 2)}")
-    st.write(f"Target season: {round(foc_num * (mins / 90), 2)}")
+    st.write(f"{focal_old_league} value: {round(start_metric, 2)}")
+    st.write(f"Possible {focal_new_league} value: {round(foc_num, 2)}")
 
 except nx.NetworkXNoPath:
     st.write(f"No path found between {focal_old_league} and {focal_new_league}")
 
-# # Plot graph
-# st.subheader("League Movement Graph")
-# options = {
-#     'node_color': 'dodgerblue',
-#     'node_size': 100,
-#     'arrowstyle': '-|>',
-#     'arrowsize': 12,
-#     'edge_cmap': 'Reds',
-#     'width': 1,
-#     'font_size': 6
-# }
-
-# net = Network(
-#     directed=True,
-#     select_menu=True,
-#     filter_menu=True,
-#     notebook=True,
-#     cdn_resources='remote'
-# )
-# net.from_nx(G)
-# net.show_buttons()
-# net.show('league_movement_graph.html')
-# st.markdown('<iframe src="league_movement_graph.html" width="100%" height="600"></iframe>', unsafe_allow_html=True)

2459110a5b3fcb79dccdb5188c9299b224368ee5
Ben Griffis <griffis.ben16@gmail.com>
2025-01-31 18:34:09 -0500

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -51,9 +51,9 @@ def make_season_metric_img(player_df, adj_80s, player, foc_var, league, season):
         tooltip=[alt.Tooltip('Opponent:N', title="Opponent"),
                  alt.Tooltip(foc_var, title=foc_var, format=".1f"),
                  alt.Tooltip('TOG%:Q', title="TOG%", format=".1f")]
-    ).properties(
-        width=1000,
-        height=800
+    # ).properties(
+    #     width=1000,
+    #     height=800
     )
 
     # Add text labels on bars

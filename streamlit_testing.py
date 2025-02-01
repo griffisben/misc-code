7729084a22da1ca3f90e8f3b2170bac738426b09
Ben Griffis <griffis.ben16@gmail.com>
2025-02-01 14:04:16 -0500

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -2718,9 +2718,10 @@ with ranking_tab:
         
         # Normalize data using z-score
         df_filtered = ranking_df.copy()
-        df_filtered[metrics] = df_filtered[metrics].apply(stats.zscore, nan_policy='omit')
         for metric in metrics:
             df_filtered[f'{metric} raw'] = df_filtered[metric]
+        df_filtered[metrics] = df_filtered[metrics].apply(stats.zscore, nan_policy='omit')
+        for metric in metrics:
             df_filtered[metric] = df_filtered[metric] + abs(df_filtered[metric].min())
             df_filtered[metric] = NormalizeData(df_filtered[metric])
             

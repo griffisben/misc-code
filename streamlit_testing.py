d12c99409232c1461957261fe19b8e89ee5f9a4e
Ben Griffis <griffis.ben16@gmail.com>
2025-02-04 16:35:13 -0500

    Update streamlit_testing.py

--- streamlit_testing.py
+++ streamlit_testing.py
@@ -119,4 +119,4 @@ for _, row in filtered_df.iterrows():
 st.pyplot(fig)
 
 filtered_df['Sequence'].fillna(method='bfill')
-filtered_df[['playerName','typeId','timeMin','timeSec','outcome','xT','xG','xGA','Sequence','Gamestate']]
+filtered_df[['playerName','typeId','timeMin','timeSec','outcome','xT','xG','xGA','Sequence','Sequence xT','Gamestate']]

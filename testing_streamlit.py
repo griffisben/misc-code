7c8c3f4791abb06393876413bda71c0b66a3984f
Ben Griffis <griffis.ben16@gmail.com>
2025-09-08 23:17:14 -0400

    Update testing_streamlit.py

--- testing_streamlit.py
+++ testing_streamlit.py
@@ -11,9 +11,10 @@ st.sidebar.success("Choose the Men's or Women's app above ☝️")
 
 st.markdown(
     """
-    ### Best XI Men's
+    ## Best XI Men's
     An all-in-one scouting tool to find & analyze players from around the world. Rank players in specific role-positions, get player radars, search for players meeting data criteria, and plot scatter plots.
-    ### Best XI Women's
+    ## Best XI Women's
     All of the features from the men's app, plus the ability to generate player similarities and create your own custom roles!
+    #### All data from Wyscout, created by Ben Griffis
 """
 )
